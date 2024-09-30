e06ed67ff824608f46eb9765b00a197a7bf524e8
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-30 15:46:25 -0500

    make favoriteNumber public

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
-    uint256 favoriteNumber; // initializes to 0 by default  
+    uint256 public favoriteNumber; // initializes to 0 by default, visibility defaults to internal
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
