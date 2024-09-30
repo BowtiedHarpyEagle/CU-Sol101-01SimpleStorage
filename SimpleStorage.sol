d9ca6e4fe70c25e939cae48ab8f02e3a56c5b6c9
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-30 15:42:12 -0500

    p7 add function store

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.18;
 
 contract SimpleStorage {
     uint256 favoriteNumber; // initializes to 0 by default  
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 }
