eceabd9898a472321d3f99664d190938782d55aa
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-30 15:33:38 -0500

    add simple types

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.18;
 
 contract SimpleStorage {
+    //Basic Types: Boolean, uint, int, address, bytes
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 88;
+    string favoriteNumberText = "eighty-eight";
+    int256 favoriteInt = -88;
+    address myAddress = 0xc314f37721AA18C757A17FCf11AEF6914Dc2b45C;
+    bytes32 favoriteBytes32 = "cat";
     
 }
