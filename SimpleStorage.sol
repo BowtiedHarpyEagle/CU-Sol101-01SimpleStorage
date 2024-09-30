aa1dd283d168d29749d29c42e080ce0c3da573de
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-30 15:37:05 -0500

    end part 6 erase other variables

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,12 +2,5 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
-    //Basic Types: Boolean, uint, int, address, bytes
-    bool hasFavoriteNumber = true;
-    uint256 favoriteNumber = 88;
-    string favoriteNumberText = "eighty-eight";
-    int256 favoriteInt = -88;
-    address myAddress = 0xc314f37721AA18C757A17FCf11AEF6914Dc2b45C;
-    bytes32 favoriteBytes32 = "cat";
-    
+    uint256 favoriteNumber; // initializes to 0 by default  
 }
