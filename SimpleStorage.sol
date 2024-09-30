5f8eccadc6b13ef240f91bfabf1a68cf0f4c233a
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-30 16:14:54 -0500

    add struct Person and Person pat

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,13 +2,21 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
-    uint256 public favoriteNumber; // initializes to 0 by default, visibility defaults to internal
+    uint256 myFavoriteNumber; // initializes to 0 by default, visibility defaults to internal
+    
+    //Struct defines a new data type Person with a favoriteNumber and a name
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    //pat is defined as a Person with favoriteNumber 7 and name "pat"
+    Person public pat = Person({favoriteNumber: 7, name: "pat"}); 
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 }
