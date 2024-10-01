0640644f740c97e7b81e0925d0af48c922ad8f16
BowtiedHarpyEagle <bowtiedharpyeagle@proton.me>
2024-09-30 19:22:15 -0500

    complete p11 mappings

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -12,12 +12,15 @@ contract SimpleStorage {
 
     Person[] public listOfPeople;
 
+    mapping (string => uint256) public nameToFavoriteNumber;
+
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push( Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
     function retrieve() public view returns(uint256) {
